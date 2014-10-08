40ae896eb0379489dd12ca5a130ea67c00b6b0a9
Oscar <oscarcaba@hotmail.com>
2014-10-08 17:56:57 -0400

    octavo commit, algunas revisiones

--- proyectojuego/proyectojuego/urls.py
+++ proyectojuego/proyectojuego/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-#from apps.usuario.views import principal
-
+from django.conf import settings
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'proyectojuego.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^',(principal)),
     url(r'^', include('proyectojuego.apps.principal.urls')),
     url(r'^', include('proyectojuego.apps.usuario.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
 )
